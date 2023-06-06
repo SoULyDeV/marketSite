4742ef453ec61fac8bab2ab0271884c9296d3888
SoULyDeV <josserand21@hotmail.com>
2023-06-06 20:46:12 +0400

    My second learning session on this project, making item views and models

--- miniMarket/item/models.py
+++ miniMarket/item/models.py
@@ -4,16 +4,16 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    
-    
+  
     def __str__(self):
         return self.name
-    
-    
-    
+
     class Meta:
         ordering = ('name',)
         verbose_name_plural = 'categories'
+
+
+
         
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
