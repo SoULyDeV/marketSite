f32f4be5044b884d7bb8c4bde7be25365ee84501
SoULyDeV <josserand21@hotmail.com>
2023-06-10 18:26:37 +0400

    add a edit function and a delete

--- miniMarket/item/views.py
+++ miniMarket/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
@@ -20,7 +20,7 @@ def detail(request, pk):
 @login_required
 def new(request):
     if request.method == 'POST':
-        form = NewItemForm(request.POST, request.FILES)
+        form = EditItemForm(request.POST, request.FILES)
         
         if form.is_valid():
             item = form.save(commit=False)
@@ -35,3 +35,31 @@ def new(request):
         'form': form,
         'title': 'New Item',
     })
+    
+@login_required
+def edit(request, pk):
+    
+    item = get_object_or_404(Item , pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES, instance=item)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
+    
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item , pk=pk, created_by=request.user)
+    item.delete()
+    
+    return redirect('dashboard:index')
